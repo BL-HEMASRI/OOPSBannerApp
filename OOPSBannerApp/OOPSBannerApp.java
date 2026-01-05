1d67e8baf7f450555f8a250fe8520e3691f3373e
BL-HEMASRI <hemasri.e@bridgelabz.com>
2026-01-05 13:29:50 +0530

    [Hemasri]-Feat:UC1: Print OOPS to Console

--- OOPSBannerApp/OOPSBannerApp.java
+++ OOPSBannerApp/OOPSBannerApp.java
@@ -1,4 +1,14 @@
-public class OOPSBannerApp{
-    
+/**
+ * OOPSBannerApp
+ *
+ * Prints the text "OOPS" to the console.
+ *
+ * @author Hema
+ * @version 1.0
+ */
+public class OOPSBannerApp {
 
+    public static void main(String[] args) {
+        System.out.println("OOPS");
+    }
 }
