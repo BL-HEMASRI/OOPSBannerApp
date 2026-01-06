3c63c9ec37db099d93ccdce51c117d3c7295dc0b
BL-HEMASRI <hemasri.e@bridgelabz.com>
2026-01-06 11:43:34 +0530

    [Hemasri]-Feat:(UC2) Render OOPS as Banner using Print Statements

--- OOPSBannerApp/OOPSBannerApp.java
+++ OOPSBannerApp/OOPSBannerApp.java
@@ -1,15 +1,13 @@
-/**
- * OOPSBannerApp
- *
- * Prints the text "OOPS" to the console.
- *
- * @author Hema
- * @version 1.0
- */
 public class OOPSBannerApp {
 
     public static void main(String[] args) {
-        System.out.println("OOPS");
+
+        System.out.println(" ********   ********   ********   ******** ");
+        System.out.println("*        * *        * *        * *         ");
+        System.out.println("*        * *        * *        * *         ");
+        System.out.println("*        * *        * ********   ******** ");
+        System.out.println("*        * *        * *                  *");
+        System.out.println("*        * *        * *                  *");
+        System.out.println(" ********   ********   *           ********");
     }
 }
-             
